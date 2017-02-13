be1f6ad33b4e14e27b1530964ebe7c9a82e10ba8
Johan van der Knijff <johan.vanderknijff@kb.nl>
2017-02-13 15:38:02 +0100

    reorganised order of functions in code

--- omSipCreator/omSipCreator.py
+++ omSipCreator/omSipCreator.py
@@ -146,7 +146,197 @@ def generate_file_sha512(fileIn):
             m.update(buf)
     return m.hexdigest()
 
-     
+def parseCommandLine():
+    # Add arguments
+    
+    # Sub-parsers for check and write commands
+
+    subparsers = parser.add_subparsers(help='sub-command help',
+                        dest='subcommand')
+    parser_verify = subparsers.add_parser('verify',
+                        help='only verify input batch without writing SIPs')
+    parser_verify.add_argument('batchIn',
+                        action="store",
+                        type=str,
+                        help="input batch")
+    parser_write = subparsers.add_parser('write',
+                        help="verify input batch and write SIPs. Before using 'write' first \
+                        run the 'verify' command and fix any reported errors.")
+    parser_write.add_argument('batchIn',
+                        action="store",
+                        type=str,
+                        help="input batch")
+    parser_write.add_argument('dirOut',
+                        action="store",
+                        type=str,
+                        help="output directory where SIPs are written")
+    # Parse arguments
+    args = parser.parse_args()
+
+    return(args)
+    
+def createMODS(PPNGroup):
+    # Create MODS metadata based on records in GGC
+    # Dublin Core to MODS mapping follows http://www.loc.gov/standards/mods/dcsimple-mods.html
+    # General structure: bibliographic md is wrapped in relatedItem / type = host element
+    
+    global errors
+    global warnings
+    
+    # Dictionary maps carrier types  to MODS resource types 
+    resourceTypeMap = {
+        "cd-rom" : "software, multimedia",
+        "dvd-rom" : "software, multimedia",
+        "dvd-video" : "moving image",
+        "cd-audio" : "sound recording"
+        }
+    
+    PPN = PPNGroup.PPN
+    carrierType = PPNGroup.carrierType
+    
+    # Create MODS element
+    modsName = etree.QName(mods_ns, "mods")
+    mods = etree.Element(modsName, nsmap = NSMAP)
+                            
+    # SRU search string (searches on dc:identifier field)
+    sruSearchString = '"PPN=' + PPN + '"'
+    response = sru.search(sruSearchString,"GGC")
+    
+    if response == False:
+        # Sru.search returns False if no match was found
+        noGGCRecords = 0
+    else:
+        noGGCRecords = response.sru.nr_of_records
+    
+    # This should return exactly one record. Return error if this is not the case
+    noGGCRecords = response.sru.nr_of_records
+    if noGGCRecords != 1:
+        logging.error("PPN " + PPN + ": search for PPN=" + PPNParent + " returned " + \
+            str(noGGCRecords) + " catalogue records (expected 1)")
+        errors += 1
+    
+    # Select first record
+    try:
+        record = next(response.records)
+        # Extract metadata
+        titles = record.titles
+        creators = record.creators
+        contributors = record.contributors
+        publishers = record.publishers
+        dates = record.dates
+        subjectsBrinkman = record.subjectsBrinkman
+        annotations = record.annotations
+        identifiersURI = record.identifiersURI
+        identifiersISBN = record.identifiersISBN
+        recordIdentifiersURI = record.recordIdentifiersURI
+        collectionIdentifiers = record.collectionIdentifiers
+    except StopIteration:
+        # Create empty lists fot all metadata fields in case noGGCRecords = 0
+        titles = []
+        creators = []
+        contributors = []
+        publishers = []
+        dates = []
+        subjectsBrinkman = []
+        annotations = []
+        identifiersURI = []
+        identifiersISBN = []
+        recordIdentifiersURI = []
+        collectionIdentifiers = []
+          
+    # Create MODS entries
+    
+    for title in titles:
+        modsTitleInfo = etree.SubElement(mods, "{%s}titleInfo" %(mods_ns))
+        modsTitle = etree.SubElement(modsTitleInfo, "{%s}title" %(mods_ns))
+        modsTitle.text = title
+          
+    for creator in creators:
+        modsName = etree.SubElement(mods, "{%s}name" %(mods_ns))
+        modsNamePart = etree.SubElement(modsName, "{%s}namePart" %(mods_ns))
+        modsNamePart.text = creator
+        modsRole =  etree.SubElement(modsName, "{%s}role" %(mods_ns))
+        modsRoleTerm =  etree.SubElement(modsRole, "{%s}roleTerm" %(mods_ns))
+        modsRoleTerm.attrib["type"] = "text"
+        modsRoleTerm.text = "creator"
+        
+    for contributor in contributors:
+        modsName = etree.SubElement(mods, "{%s}name" %(mods_ns))
+        modsNamePart = etree.SubElement(modsName, "{%s}namePart" %(mods_ns))
+        modsNamePart.text = contributor
+        modsRole =  etree.SubElement(modsName, "{%s}role" %(mods_ns))
+        modsRoleTerm =  etree.SubElement(modsRole, "{%s}roleTerm" %(mods_ns))
+        modsRoleTerm.attrib["type"] = "text"
+        modsRoleTerm.text = "contributor"
+    
+    for publisher in publishers:
+        modsOriginInfo = etree.SubElement(mods, "{%s}originInfo" %(mods_ns))
+        modsOriginInfo.attrib["displayLabel"] = "publisher"
+        modsPublisher = etree.SubElement(modsOriginInfo, "{%s}publisher" %(mods_ns))
+        modsPublisher.text = publisher
+                 
+    for date in dates:
+        # Note that DC date isn't necessarily issue date, and LoC DC to MODS mapping
+        # suggests that dateOther be used as default. However KB Metadata model
+        # only recognises dateIssued, so we'll use that. 
+        modsOriginInfo = etree.SubElement(mods, "{%s}originInfo" %(mods_ns))
+        modsDateIssued = etree.SubElement(modsOriginInfo, "{%s}dateIssued" %(mods_ns))
+        modsDateIssued.text = date
+    
+    # TODO: perhaps add authority and language attributes
+    modsSubject = etree.SubElement(mods, "{%s}subject" %(mods_ns))    
+    for subjectBrinkman in subjectsBrinkman:
+        modsTopic = etree.SubElement(modsSubject, "{%s}topic" %(mods_ns))
+        modsTopic.text = subjectBrinkman
+        
+    modsTypeOfResource = etree.SubElement(mods, "{%s}typeOfResource" %(mods_ns))
+    modsTypeOfResource.text = resourceTypeMap[carrierType]
+
+    for annotation in annotations:
+        modsNote = etree.SubElement(mods, "{%s}note" %(mods_ns))
+        modsNote.text = annotation
+    
+    # This record establishes the link with the parent publication as it is described
+    # in the GGC
+    modsRelatedItem = etree.SubElement(mods, "{%s}relatedItem" %(mods_ns))
+    modsRelatedItem.attrib["type"] = "host"
+
+    modsIdentifierPPN = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
+    modsIdentifierPPN.attrib["type"] = "ppn"
+    modsIdentifierPPN.text = PPN
+    
+    # NOTE: GGC record contain 2 URI- type identifiers:
+    # 1. dc:identifier with URI of form: http://resolver.kb.nl/resolve?urn=PPN:236599380 (OpenURL?)
+    # 2. dcx:recordIdentifier with URI of form: http://opc4.kb.nl/DB=1/PPN?PPN=236599380
+    # URL 1. resolves to URL2, but not sure which one is more persistent?
+    # Also a MODS RecordIdentifier field does exist, but it doesn't have a 'type' attribute
+    # so we cannot specify it is a URI. For now both are included as 'identifier' elements
+    #
+    
+    for identifierURI in identifiersURI:
+        modsIdentifierURI = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
+        modsIdentifierURI.attrib["type"] = "uri"
+        modsIdentifierURI.text = identifierURI
+    """
+    for recordIdentifierURI in recordIdentifiersURI:
+        modsIdentifierURI = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
+        modsIdentifierURI.attrib["type"] = "uri"
+        modsIdentifierURI.text = recordIdentifierURI
+    """
+    
+    for identifierISBN in identifiersISBN:
+        modsIdentifierISBN = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
+        modsIdentifierISBN.attrib["type"] = "isbn"
+        modsIdentifierISBN.text = identifierISBN
+   
+    # Add some info on how MODS was generated
+    modsRecordInfo = etree.SubElement(mods, "{%s}recordInfo" %(mods_ns))
+    modsRecordOrigin = etree.SubElement(modsRecordInfo, "{%s}recordOrigin" %(mods_ns))
+    originText = "Automatically generated by " + scriptName + " v. " + __version__  + " from records in KB Catalogue."
+    modsRecordOrigin.text = originText
+      
+    return(mods)
+       
 def processCarrier(carrier, fileGrp, SIPPath, sipFileCounterStart):
     # Process contents of imagepath directory
     # TODO: * check file type / extension matches carrierType!
@@ -326,198 +516,6 @@ def processCarrier(carrier, fileGrp, SIPPath, sipFileCounterStart):
         # Dummy value, not used
         divDisc = etree.Element('rubbish')
     return(fileGrp, divDisc, sipFileCounter)             
-                
-   
-def createMODS(PPNGroup):
-    # Create MODS metadata based on records in GGC
-    # Dublin Core to MODS mapping follows http://www.loc.gov/standards/mods/dcsimple-mods.html
-    # General structure: bibliographic md is wrapped in relatedItem / type = host element
-    
-    global errors
-    global warnings
-    
-    # Dictionary maps carrier types  to MODS resource types 
-    resourceTypeMap = {
-        "cd-rom" : "software, multimedia",
-        "dvd-rom" : "software, multimedia",
-        "dvd-video" : "moving image",
-        "cd-audio" : "sound recording"
-        }
-    
-    PPN = PPNGroup.PPN
-    carrierType = PPNGroup.carrierType
-    
-    # Create MODS element
-    modsName = etree.QName(mods_ns, "mods")
-    mods = etree.Element(modsName, nsmap = NSMAP)
-                            
-    # SRU search string (searches on dc:identifier field)
-    sruSearchString = '"PPN=' + PPN + '"'
-    response = sru.search(sruSearchString,"GGC")
-    
-    if response == False:
-        # Sru.search returns False if no match was found
-        noGGCRecords = 0
-    else:
-        noGGCRecords = response.sru.nr_of_records
-    
-    # This should return exactly one record. Return error if this is not the case
-    noGGCRecords = response.sru.nr_of_records
-    if noGGCRecords != 1:
-        logging.error("PPN " + PPN + ": search for PPN=" + PPNParent + " returned " + \
-            str(noGGCRecords) + " catalogue records (expected 1)")
-        errors += 1
-    
-    # Select first record
-    try:
-        record = next(response.records)
-        # Extract metadata
-        titles = record.titles
-        creators = record.creators
-        contributors = record.contributors
-        publishers = record.publishers
-        dates = record.dates
-        subjectsBrinkman = record.subjectsBrinkman
-        annotations = record.annotations
-        identifiersURI = record.identifiersURI
-        identifiersISBN = record.identifiersISBN
-        recordIdentifiersURI = record.recordIdentifiersURI
-        collectionIdentifiers = record.collectionIdentifiers
-    except StopIteration:
-        # Create empty lists fot all metadata fields in case noGGCRecords = 0
-        titles = []
-        creators = []
-        contributors = []
-        publishers = []
-        dates = []
-        subjectsBrinkman = []
-        annotations = []
-        identifiersURI = []
-        identifiersISBN = []
-        recordIdentifiersURI = []
-        collectionIdentifiers = []
-          
-    # Create MODS entries
-    
-    for title in titles:
-        modsTitleInfo = etree.SubElement(mods, "{%s}titleInfo" %(mods_ns))
-        modsTitle = etree.SubElement(modsTitleInfo, "{%s}title" %(mods_ns))
-        modsTitle.text = title
-          
-    for creator in creators:
-        modsName = etree.SubElement(mods, "{%s}name" %(mods_ns))
-        modsNamePart = etree.SubElement(modsName, "{%s}namePart" %(mods_ns))
-        modsNamePart.text = creator
-        modsRole =  etree.SubElement(modsName, "{%s}role" %(mods_ns))
-        modsRoleTerm =  etree.SubElement(modsRole, "{%s}roleTerm" %(mods_ns))
-        modsRoleTerm.attrib["type"] = "text"
-        modsRoleTerm.text = "creator"
-        
-    for contributor in contributors:
-        modsName = etree.SubElement(mods, "{%s}name" %(mods_ns))
-        modsNamePart = etree.SubElement(modsName, "{%s}namePart" %(mods_ns))
-        modsNamePart.text = contributor
-        modsRole =  etree.SubElement(modsName, "{%s}role" %(mods_ns))
-        modsRoleTerm =  etree.SubElement(modsRole, "{%s}roleTerm" %(mods_ns))
-        modsRoleTerm.attrib["type"] = "text"
-        modsRoleTerm.text = "contributor"
-    
-    for publisher in publishers:
-        modsOriginInfo = etree.SubElement(mods, "{%s}originInfo" %(mods_ns))
-        modsOriginInfo.attrib["displayLabel"] = "publisher"
-        modsPublisher = etree.SubElement(modsOriginInfo, "{%s}publisher" %(mods_ns))
-        modsPublisher.text = publisher
-                 
-    for date in dates:
-        # Note that DC date isn't necessarily issue date, and LoC DC to MODS mapping
-        # suggests that dateOther be used as default. However KB Metadata model
-        # only recognises dateIssued, so we'll use that. 
-        modsOriginInfo = etree.SubElement(mods, "{%s}originInfo" %(mods_ns))
-        modsDateIssued = etree.SubElement(modsOriginInfo, "{%s}dateIssued" %(mods_ns))
-        modsDateIssued.text = date
-    
-    # TODO: perhaps add authority and language attributes
-    modsSubject = etree.SubElement(mods, "{%s}subject" %(mods_ns))    
-    for subjectBrinkman in subjectsBrinkman:
-        modsTopic = etree.SubElement(modsSubject, "{%s}topic" %(mods_ns))
-        modsTopic.text = subjectBrinkman
-        
-    modsTypeOfResource = etree.SubElement(mods, "{%s}typeOfResource" %(mods_ns))
-    modsTypeOfResource.text = resourceTypeMap[carrierType]
-
-    for annotation in annotations:
-        modsNote = etree.SubElement(mods, "{%s}note" %(mods_ns))
-        modsNote.text = annotation
-    
-    # This record establishes the link with the parent publication as it is described
-    # in the GGC
-    modsRelatedItem = etree.SubElement(mods, "{%s}relatedItem" %(mods_ns))
-    modsRelatedItem.attrib["type"] = "host"
-
-    modsIdentifierPPN = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
-    modsIdentifierPPN.attrib["type"] = "ppn"
-    modsIdentifierPPN.text = PPN
-    
-    # NOTE: GGC record contain 2 URI- type identifiers:
-    # 1. dc:identifier with URI of form: http://resolver.kb.nl/resolve?urn=PPN:236599380 (OpenURL?)
-    # 2. dcx:recordIdentifier with URI of form: http://opc4.kb.nl/DB=1/PPN?PPN=236599380
-    # URL 1. resolves to URL2, but not sure which one is more persistent?
-    # Also a MODS RecordIdentifier field does exist, but it doesn't have a 'type' attribute
-    # so we cannot specify it is a URI. For now both are included as 'identifier' elements
-    #
-    
-    for identifierURI in identifiersURI:
-        modsIdentifierURI = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
-        modsIdentifierURI.attrib["type"] = "uri"
-        modsIdentifierURI.text = identifierURI
-    """
-    for recordIdentifierURI in recordIdentifiersURI:
-        modsIdentifierURI = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
-        modsIdentifierURI.attrib["type"] = "uri"
-        modsIdentifierURI.text = recordIdentifierURI
-    """
-    
-    for identifierISBN in identifiersISBN:
-        modsIdentifierISBN = etree.SubElement(modsRelatedItem, "{%s}identifier" %(mods_ns))
-        modsIdentifierISBN.attrib["type"] = "isbn"
-        modsIdentifierISBN.text = identifierISBN
-   
-    # Add some info on how MODS was generated
-    modsRecordInfo = etree.SubElement(mods, "{%s}recordInfo" %(mods_ns))
-    modsRecordOrigin = etree.SubElement(modsRecordInfo, "{%s}recordOrigin" %(mods_ns))
-    originText = "Automatically generated by " + scriptName + " v. " + __version__  + " from records in KB Catalogue."
-    modsRecordOrigin.text = originText
-      
-    return(mods)
-       
-def parseCommandLine():
-    # Add arguments
-    
-    # Sub-parsers for check and write commands
-
-    subparsers = parser.add_subparsers(help='sub-command help',
-                        dest='subcommand')
-    parser_verify = subparsers.add_parser('verify',
-                        help='only verify input batch without writing SIPs')
-    parser_verify.add_argument('batchIn',
-                        action="store",
-                        type=str,
-                        help="input batch")
-    parser_write = subparsers.add_parser('write',
-                        help="verify input batch and write SIPs. Before using 'write' first \
-                        run the 'verify' command and fix any reported errors.")
-    parser_write.add_argument('batchIn',
-                        action="store",
-                        type=str,
-                        help="input batch")
-    parser_write.add_argument('dirOut',
-                        action="store",
-                        type=str,
-                        help="output directory where SIPs are written")
-    # Parse arguments
-    args = parser.parse_args()
-
-    return(args)
     
 def processPPN(PPN, carriers, dirOut, colsBatchManifest, batchIn, dirsInMetaCarriers, carrierTypeAllowedValues):
 
